c3c6b14bede40b5220023c478814872e6eaa2bc4
Naman Jain <naman1205jain@gmail.com>
2018-10-13 18:57:11 +0530

    insert.sql

---
+++ insert.sql
@@ -0,0 +1,17 @@
+insert into users values ('101', '000111', '111000');
+insert into users values ('102', '000222', '222000');
+
+insert into department values ('CS');
+
+insert into instructor values('101', 'Sir', 'CS');
+
+insert into student values('16005000', 'Ideal_guy', '102', 'CS');
+
+insert into course values('CS101', 'Intro to CS', 'CS');
+
+insert into section values('CS101', 'Autumn', '2018');
+
+insert into teaches values('101', 'CS101', 'Autumn', '2018');
+
+
+
